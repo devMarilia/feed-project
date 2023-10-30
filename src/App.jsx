f2a7fc8429e7d492f6a8215859f808d5c0edc94b
devMarilia <devMarilia.frontend@gmail.com>
2023-10-30 18:13:06 -0300

    ✨ feat: Component Sidebar and Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./global.css";
-import { Header, Post } from "./components";
+import styles from './App.module.css'
+import { Header, Post, Sidebar } from "./components";
+
 
 export const App = () => {
   return (
-    <>
+    <div>
       <Header />
-      <Post/>
-    </>
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+        <main>
+          <Post
+            author="Dev Lia"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo perferendis autem a nam sunt adipisci voluptas voluptatum officiis iusto sint, tenetur alias, placeat eligendi debitis dignissimos laborum recusandae ipsam at."
+          />
+        </main>
+      </div>
+    </div>
   );
 };
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,6 @@
+import styles from './Sidebar.module.css'
+export const Sidebar = () => {
+    return (
+        <aside className={styles.sidebar}>Teste</aside>
+    )
+}
